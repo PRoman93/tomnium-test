372017ca71065154ca111f70ae08239edbdfd089
PRoman93 <roman_prokopets@mail.ua>
2020-06-03 17:46:03 +0300

    Updates

---
+++ precache-manifest.1c821a051e6eacae12546fa36bb6f629.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f7d1afa1a5023f6ab5fec7f3aa860e6",
+    "url": "/tomnium-test/index.html"
+  },
+  {
+    "revision": "8701c9e66a554b07d3e6",
+    "url": "/tomnium-test/static/css/main.c00e23d8.chunk.css"
+  },
+  {
+    "revision": "9ddf1b917201e6f65ad6",
+    "url": "/tomnium-test/static/js/2.916da629.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/tomnium-test/static/js/2.916da629.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8701c9e66a554b07d3e6",
+    "url": "/tomnium-test/static/js/main.b93b96ea.chunk.js"
+  },
+  {
+    "revision": "72910a025c2f90b3e21c",
+    "url": "/tomnium-test/static/js/runtime-main.5712f658.js"
+  }
+]);
