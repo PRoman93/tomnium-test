14db63566fc1180943a19b7f626e98f6941c972f
PRoman93 <roman_prokopets@mail.ua>
2020-06-03 17:21:05 +0300

    first commit

---
+++ src/ui/App.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+    return (
+        <div className="App">
+            <table>
+                <tbody>
+                <tr>
+                    <th>1</th>
+                </tr>
+                <tr>
+                    <td>2</td>
+                </tr>
+                </tbody>
+            </table>
+        </div>
+    );
+}
+
+export default App;
